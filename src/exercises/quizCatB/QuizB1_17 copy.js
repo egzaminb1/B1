3c9be3080e6cce87bb12207edcf0bdfd0c06bdf4
egzaminb1 <WebWonderLocker@gmail.com>
2022-02-12 23:17:45 +0100

    exam test one finished

---
+++ src/exercises/quizCatB/QuizB1_17 copy.js
@@ -0,0 +1,10 @@
+
+
+export const QuizB1_17 = {
+    header:['Proszę uzupełnić tekst poprawną formą wyrazów w nawiasach zgodnie z podanym przykładem (stopniowanie przymiotników).',''],
+    
+    template:{  point:0.5},
+    text:[``],
+        
+    answer:['wygodniejsze', '', '', '', '', '', '', '', '', '', '']
+}
